ef58545f49bee8b1777d5c2c45b8f96c95347568
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2018-08-02 20:02:48 +0300

    [wsserver_test] Refactoring
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- wsserver/wsserver_test.go
+++ wsserver/wsserver_test.go
@@ -8,8 +8,6 @@ import (
 	"time"
 
 	"github.com/godbus/dbus"
-	//	"time"
-
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
 
@@ -59,8 +57,6 @@ func init() {
 }
 
 func (GetPermission dbusInterface) GetPermission(token string) (string, string, *dbus.Error) {
-	log.Info("[TEST] GetPermission token: ", token)
-
 	return `{"Signal.Test.RPM": "r"}`, "OK", nil
 }
 
@@ -71,19 +67,18 @@ func (GetPermission dbusInterface) GetPermission(token string) (string, string, 
 func TestMain(m *testing.M) {
 	conn, err := dbus.SessionBus()
 	if err != nil {
-		log.Errorf("Can't create session connection: %v", err)
-		os.Exit(1)
+		log.Fatalf("Can't create session connection: %s", err)
 	}
-	reply, err := conn.RequestName("com.aosservicemanager.vistoken",
-		dbus.NameFlagDoNotQueue)
+
+	reply, err := conn.RequestName("com.aosservicemanager.vistoken", dbus.NameFlagDoNotQueue)
 	if err != nil {
-		log.Error("Can't RequestName")
-		os.Exit(1)
+		log.Fatal("Can't request name")
 	}
+
 	if reply != dbus.RequestNameReplyPrimaryOwner {
-		log.Error("Name already taken")
-		os.Exit(1)
+		log.Fatal("Name already taken")
 	}
+
 	dbusserver := dbusInterface{}
 	conn.Export(dbusserver, "/com/aosservicemanager/vistoken", "com.aosservicemanager.vistoken")
 
@@ -93,16 +88,13 @@ func TestMain(m *testing.M) {
 	}
 
 	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&servConfig)
-	if err != nil {
-		log.Error("Error parsing visconfig.json: ", err)
-		os.Exit(1)
+	if err = decoder.Decode(&servConfig); err != nil {
+		log.Fatalf("Error parsing visconfig.json: %s", err)
 	}
 
 	server, err := wsserver.New(servConfig.ServerUrl, servConfig.VISCert, servConfig.VISKey)
 	if err != nil {
-		log.Fatal("Can't create ws server ", err)
-		return
+		log.Fatalf("Can't create ws server: %s", err)
 	}
 
 	// There is raise condition: after new listen is not started yet
@@ -125,312 +117,256 @@ func TestGetNoAuth(t *testing.T) {
 	u := url.URL{Scheme: "wss", Host: servConfig.ServerUrl, Path: "/"}
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		t.Fatalf("Can't connect to ws server %v", err)
+		t.Fatalf("Can't connect to ws server %s", err)
 	}
 	defer closeConnection(c)
 
 	getmessage := `{"action": "get", "path": "Attribute.Vehicle.VehicleIdentification.VIN", "requestId": "8756"}`
-	err = c.WriteMessage(websocket.TextMessage, []byte(getmessage))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
+	if err = c.WriteMessage(websocket.TextMessage, []byte(getmessage)); err != nil {
+		t.Fatalf("Can't send message to server %s", err)
 	}
 
 	_, message, err := c.ReadMessage()
 	if err != nil {
-		t.Fatalf("Can't read message from server %v", err)
+		t.Fatalf("Can't read message from server %s", err)
 	}
 
 	var resp visResponse
 
-	err = json.Unmarshal(message, &resp)
-	if err != nil {
-		t.Fatalf("Error parcing get response: %v", err)
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing get response: %s", err)
 	}
 
 	if (resp.Action != "get") || (resp.RequestID != "8756") {
 		t.Fatalf("Unexpected value")
 	}
 	if resp.Error != nil {
-		t.Fatalf("Error parsing get request:  %v", err)
+		t.Fatalf("Error parsing get request:  %s", err)
 	}
 }
 
 func TestSet(t *testing.T) {
-	log.Debug("[TEST] TestSet")
-
 	u := url.URL{Scheme: "wss", Host: servConfig.ServerUrl, Path: "/"}
-	log.Debug("[TEST] Connecting to ", u.String())
-
-	time.Sleep(time.Second)
-
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		t.Fatalf("Can't connect to ws server %v", err)
-		return
+		t.Fatalf("Can't connect to ws server %s", err)
 	}
-	defer c.Close()
-	getmessage := `{"action": "set", "path": "Attribute.Emulator.*", "value": [ {"rectangle_long0": 100 },
-	{"rectangle_lat0": 150}, {"rectangle_long1": 200 }, {"rectangle_lat1": 250} ], "requestId": "8888"}`
+	defer closeConnection(c)
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(getmessage))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
-		return
+	getmessage := `{"action": "set", "path": "Attribute.Emulator.*", "value": [ {"rectangle_long0": 100 },` +
+		`{"rectangle_lat0": 150}, {"rectangle_long1": 200 }, {"rectangle_lat1": 250} ], "requestId": "8888"}`
+
+	if err = c.WriteMessage(websocket.TextMessage, []byte(getmessage)); err != nil {
+		t.Fatalf("Can't send message to server %s", err)
 	}
 	_, message, err := c.ReadMessage()
 	if err != nil {
-		t.Fatalf("Can't read message from server %v", err)
-		return
+		t.Fatalf("Can't read message from server %s", err)
 	}
-	log.Debug("[TEST] read:", string(message))
 
 	var resp visResponse
-	err = json.Unmarshal(message, &resp)
-	if err != nil {
-		t.Fatalf("Error parcing set response: %v", err)
-		return
+
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing set response: %s", err)
 	}
 
 	if (resp.Action != "set") || (resp.RequestID != "8888") {
 		t.Fatalf("Unexpected value")
 	}
 	if resp.Error != nil {
-		t.Fatalf("Error parsing get request:  %v", err)
+		t.Fatalf("Error parsing get request:  %s", err)
 	}
 }
 
 func TestGetWithAuth(t *testing.T) {
-	log.Debug("[TEST] TestGetWithAuth")
-
-	var resp visResponse
-
 	u := url.URL{Scheme: "wss", Host: servConfig.ServerUrl, Path: "/"}
-	log.Debug("[TEST] Connecting to ", u.String())
-
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		t.Fatalf("Can't connect to ws server %v", err)
-		return
+		t.Fatalf("Can't connect to ws server %s", err)
 	}
-	defer c.Close()
+	defer closeConnection(c)
+
 	//--------------- send GET wait for error 403
+
 	requestMsg := `{"action": "get", "path": "Signal.Test.RPM", "requestId": "8755"}`
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(requestMsg))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
-		return
+	if err = c.WriteMessage(websocket.TextMessage, []byte(requestMsg)); err != nil {
+		t.Fatalf("Can't send message to server %s", err)
 	}
 
 	_, message, err := c.ReadMessage()
 	if err != nil {
-		t.Fatalf("Can't read message from server: %v", err)
-		return
+		t.Fatalf("Can't read message from server: %s", err)
 	}
-	log.Debug("[TEST] read:", string(message))
 
-	err = json.Unmarshal(message, &resp)
-	if err != nil {
-		t.Fatalf("Error parsing get response: %v", err)
-		return
+	var resp visResponse
+
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing get response: %s", err)
 	}
 
 	if (resp.Action != "get") || (resp.RequestID != "8755") {
 		t.Fatalf("Unexpected value")
 	}
-
-	if resp.Error == nil {
-		t.Fatalf("Should be error 403")
-		return
-	}
-	if resp.Error.Number != 403 {
-		t.Fatalf("Should be error 403")
-		return
+	if resp.Error == nil || resp.Error.Number != 401 {
+		t.Fatalf("Should be error 401")
 	}
 
 	//-------------- send authorize request wait OK
 
 	requestMsg = `{"action": "authorize", "tokens" : {"authorization" : "appUID" }, "requestId": "12345"}`
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(requestMsg))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
-		return
+	if err = c.WriteMessage(websocket.TextMessage, []byte(requestMsg)); err != nil {
+		t.Fatalf("Can't send message to server %s", err)
 	}
+
 	_, message, err = c.ReadMessage()
 	if err != nil {
-		t.Fatalf("Can't read message from server: %v", err)
-		return
-	}
-	log.Debug("[TEST] read:", string(message))
-
-	var resp2 visResponse
-	err = json.Unmarshal(message, &resp2)
-	if err != nil {
-		t.Fatalf("Error parcing authorization response: %v", err)
-		return
+		t.Fatalf("Can't read message from server: %s", err)
 	}
 
-	if (resp2.Action != "authorize") || (resp2.RequestID != "12345") {
+	resp = visResponse{}
+
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing authorization response: %s", err)
+	}
+
+	if (resp.Action != "authorize") || (resp.RequestID != "12345") {
 		t.Fatalf("Unexpected value")
 	}
-
-	log.Debug("[TEST] read: ", resp2)
-	if resp2.Error != nil {
-		t.Fatalf("Error authorize %v", resp2.Error.Number)
+	if resp.Error != nil {
+		t.Fatalf("Error authorize: %s", resp.Error.Message)
 	}
 
 	requestMsg = `{"action": "get", "path": "Signal.Test.RPM", "requestId": "12347"}`
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(requestMsg))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
-		return
-	}
-	_, message, err = c.ReadMessage()
-	log.Debug("[TEST] read:", string(message))
-	if err != nil {
-		t.Fatalf("Can't read message from server: %v", err)
-		return
-	}
-	var resp3 visResponse
-	err = json.Unmarshal(message, &resp3)
-	if err != nil {
-		t.Fatalf("Error parcing get response: %v", err)
-		return
+	if err = c.WriteMessage(websocket.TextMessage, []byte(requestMsg)); err != nil {
+		t.Fatalf("Can't send message to server: %s", err)
 	}
 
-	if (resp3.Action != "get") || (resp3.RequestID != "12347") {
+	_, message, err = c.ReadMessage()
+	if err != nil {
+		t.Fatalf("Can't read message from server: %s", err)
+	}
+
+	resp = visResponse{}
+
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing get response: %s", err)
+	}
+
+	if (resp.Action != "get") || (resp.RequestID != "12347") {
 		t.Fatalf("Unexpected value")
 	}
 
-	if resp3.Error != nil {
-		t.Fatalf("Error get %v", resp3.Error.Number)
+	if resp.Error != nil {
+		t.Fatalf("Error get: %s", resp.Error.Message)
 	}
-
-	defer c.Close()
 }
 
 func TestSubscribeUnsubscribe(t *testing.T) {
-	log.Debug("[TEST] TestSubscribeUnsubscribe")
-
 	u := url.URL{Scheme: "wss", Host: servConfig.ServerUrl, Path: "/"}
-	log.Debug("[TEST] Connecting to ", u.String())
-
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		t.Fatalf("Can't connect to ws server %v", err)
-		return
+		t.Fatalf("Can't connect to ws server: %s", err)
 	}
-	defer c.Close()
+	defer closeConnection(c)
 
 	subscMessage := `{"action": "subscribe","path": "Attribute.Vehicle.VehicleIdentification.VIN", "requestId": "1004"}`
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(subscMessage))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
-		return
+	if err = c.WriteMessage(websocket.TextMessage, []byte(subscMessage)); err != nil {
+		t.Fatalf("Can't send message to server: %s", err)
 	}
+
 	_, message, err := c.ReadMessage()
 	if err != nil {
-		t.Fatalf("Can't read message from server: %v", err)
-		return
+		t.Fatalf("Can't read message from server: %s", err)
 	}
-	log.Debug("[TEST] read:", string(message))
 
 	var resp visResponse
-	err = json.Unmarshal(message, &resp)
-	if err != nil {
-		t.Fatalf("Error parcing subscribe response: %v", err)
-		return
+
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing subscribe response: %s", err)
 	}
 
 	if (resp.Action != "subscribe") || (resp.RequestID != "1004") {
 		t.Fatalf("Unexpected value")
 	}
 	if resp.Error != nil {
-		t.Fatalf("Unexpected error for subscribe  %v", err)
+		t.Fatalf("Unexpected error for subscribe: %s", resp.Error.Message)
 	}
 	if resp.SubscriptionID == nil {
 		t.Fatalf("No subscriptionId")
 	}
 
-	unsubscMessage := `{"action": "unsubscribe", "subscriptionId": "0000", "requestId": "1004"}`
+	unsubscMessage := `{"action": "unsubscribe", "subscriptionId": "0", "requestId": "1004"}`
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(unsubscMessage))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
-		return
+	if err = c.WriteMessage(websocket.TextMessage, []byte(unsubscMessage)); err != nil {
+		t.Fatalf("Can't send message to server: %s", err)
 	}
-	_, message2, err := c.ReadMessage()
+	_, message, err = c.ReadMessage()
 	if err != nil {
-		t.Fatalf("Can't read message from server: %v", err)
-		return
-	}
-	log.Debug("[TEST] read:", string(message2))
-
-	var resp2 visResponse
-	err = json.Unmarshal(message2, &resp2)
-	if err != nil {
-		t.Fatalf("Error parcing unsubscribe response: %v", err)
-		return
+		t.Fatalf("Can't read message from server: %s", err)
 	}
 
-	if (resp2.Action != "unsubscribe") || (resp2.RequestID != "1004") {
+	resp = visResponse{}
+
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing unsubscribe response: %s", err)
+	}
+
+	if (resp.Action != "unsubscribe") || (resp.RequestID != "1004") {
 		t.Fatalf("Unexpected value")
 	}
-	if resp2.Error == nil {
-		t.Fatalf("Unexpected positive responce ")
+	if resp.Error == nil {
+		t.Fatal("Unexpected positive response")
 	}
 
 	unsubscMessageOK := `{"action": "unsubscribe", "subscriptionId": "1", "requestId": "1004"}`
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(unsubscMessageOK))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
-	}
-	_, message3, err := c.ReadMessage()
-	if err != nil {
-		t.Fatalf("Can't read message from server: %v", err)
-	}
-	log.Debug("[TEST] read:", string(message3))
-
-	var resp3 visResponse
-	err = json.Unmarshal(message3, &resp3)
-	if err != nil {
-		t.Fatalf("Error parcing unsubscribe response: %v", err)
+	if err = c.WriteMessage(websocket.TextMessage, []byte(unsubscMessageOK)); err != nil {
+		t.Fatalf("Can't send message to server %s", err)
 	}
 
-	if (resp3.Action != "unsubscribe") || (resp3.RequestID != "1004") {
+	_, message, err = c.ReadMessage()
+	if err != nil {
+		t.Fatalf("Can't read message from server: %s", err)
+	}
+
+	resp = visResponse{}
+
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing unsubscribe response: %s", err)
+	}
+
+	if (resp.Action != "unsubscribe") || (resp.RequestID != "1004") {
 		t.Fatalf("Unexpected value")
 	}
-	if resp3.Error != nil {
-		t.Fatalf("Unexpected error for Unsubscribe  %v", err)
+	if resp.Error != nil {
+		t.Fatalf("Unexpected error for unsubscribe  %s", resp.Error.Message)
 	}
 
 	unsubscMessageAll := `{"action": "unsubscribeAll", "requestId": "1004"}`
 
-	err = c.WriteMessage(websocket.TextMessage, []byte(unsubscMessageAll))
-	if err != nil {
-		t.Fatalf("Can't send message to server %v", err)
-		return
-	}
-	_, messageAll, err := c.ReadMessage()
-	if err != nil {
-		t.Fatalf("Can't read message from server: %v", err)
-	}
-	log.Debug("[TEST] read:", string(messageAll))
-
-	var respAll visResponse
-	err = json.Unmarshal(messageAll, &respAll)
-	if err != nil {
-		t.Fatalf("Error parcing unsubscribe response: %v", err)
+	if err = c.WriteMessage(websocket.TextMessage, []byte(unsubscMessageAll)); err != nil {
+		t.Fatalf("Can't send message to server: %s", err)
 	}
 
-	if (respAll.Action != "unsubscribeAll") || (resp3.RequestID != "1004") {
+	_, message, err = c.ReadMessage()
+	if err != nil {
+		t.Fatalf("Can't read message from server: %s", err)
+	}
+
+	resp = visResponse{}
+
+	if err = json.Unmarshal(message, &resp); err != nil {
+		t.Fatalf("Error parsing unsubscribe response: %s", err)
+	}
+
+	if (resp.Action != "unsubscribeAll") || (resp.RequestID != "1004") {
 		t.Fatalf("Unexpected value")
 	}
-	if resp3.Error != nil {
-		t.Fatalf("Unexpected error for Unsubscribe All %v", err)
+	if resp.Error != nil {
+		t.Fatalf("Unexpected error for unsubscribe all: %s", resp.Error.Message)
 	}
 }
