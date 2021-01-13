891df58524fe94c969ae48afa9bfbaeca23dcc1d
Yevgen Abramov <Yevgen_Abramov@epam.com>
2021-01-13 14:52:51 +0200

    [boardmodel] Add board model adapter
    Signed-off-by: Yevgen Abramov <Yevgen_Abramov@epam.com>

---
+++ plugins/boardmodeladapter/boardmodeladapter.go
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2021 Renesas Inc.
+// Copyright 2021 EPAM Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package boardmodeladapter
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	log "github.com/sirupsen/logrus"
+
+	"aos_vis/dataprovider"
+)
+
+/*******************************************************************************
+ * Consts
+ ******************************************************************************/
+
+const defaultBoardModel = "board:1.0"
+
+/*******************************************************************************
+ * Types
+ ******************************************************************************/
+
+type boardModelAdapter struct {
+	boardModel string
+	config     adapterConfig
+}
+
+type adapterConfig struct {
+	VISPath  string `json:"VISPath"`
+	FilePath string `json:"FilePath"`
+}
+
+/*******************************************************************************
+ * Public
+ ******************************************************************************/
+
+// New creates adapter instance
+func New(configJSON json.RawMessage) (adapter dataprovider.DataAdapter, err error) {
+	log.Info("Create BoardModel adapter")
+
+	localAdapter := new(boardModelAdapter)
+
+	if configJSON == nil {
+		return nil, errors.New("config should be set")
+	}
+
+	if err = json.Unmarshal(configJSON, &localAdapter.config); err != nil {
+		return nil, err
+	}
+
+	boardModel, err := ioutil.ReadFile(localAdapter.config.FilePath)
+	if err != nil {
+		log.Warnf("Can't read board model: %s. Use default one: %s", err, defaultBoardModel)
+
+		if err = ioutil.WriteFile(localAdapter.config.FilePath, []byte(defaultBoardModel), 0644); err != nil {
+			return nil, err
+		}
+
+		boardModel = []byte(defaultBoardModel)
+	}
+
+	localAdapter.boardModel = string(boardModel)
+
+	log.WithField("Board model", localAdapter.boardModel).Debug("BoardModel adapter")
+
+	return localAdapter, nil
+}
+
+// Close closes adapter
+func (adapter *boardModelAdapter) Close() {
+	log.Info("Close BoardModel adapter")
+}
+
+// GetName returns adapter name
+func (adapter *boardModelAdapter) GetName() (name string) {
+	return "boardmodeladapter"
+}
+
+// GetPathList returns list of all pathes for this adapter
+func (adapter *boardModelAdapter) GetPathList() (pathList []string, err error) {
+	return []string{adapter.config.VISPath}, nil
+}
+
+// IsPathPublic returns true if requested data accessible without authorization
+func (adapter *boardModelAdapter) IsPathPublic(path string) (result bool, err error) {
+	return true, nil
+}
+
+// GetData returns data by path
+func (adapter *boardModelAdapter) GetData(pathList []string) (data map[string]interface{}, err error) {
+	data = make(map[string]interface{})
+
+	for _, path := range pathList {
+		if path == adapter.config.VISPath {
+			data[path] = adapter.boardModel
+		} else {
+			return nil, fmt.Errorf("path %s doesn't exits", path)
+		}
+	}
+
+	return data, nil
+}
+
+// SetData sets data by pathes
+func (adapter *boardModelAdapter) SetData(data map[string]interface{}) (err error) {
+	if len(data) == 0 {
+		return nil
+	}
+
+	for _, path := range data {
+		if path != adapter.config.VISPath {
+			return fmt.Errorf("path %s doesn't exits", path)
+		}
+	}
+
+	return fmt.Errorf("signal %s cannot be set since it is a read only attribute", adapter.config.VISPath)
+}
+
+// GetSubscribeChannel returns channel on which data changes will be sent
+func (adapter *boardModelAdapter) GetSubscribeChannel() (channel <-chan map[string]interface{}) {
+	return nil
+}
+
+// Subscribe subscribes for data changes
+func (adapter *boardModelAdapter) Subscribe(pathList []string) (err error) {
+	return nil
+}
+
+// Unsubscribe unsubscribes from data changes
+func (adapter *boardModelAdapter) Unsubscribe(pathList []string) (err error) {
+	return nil
+}
+
+// UnsubscribeAll unsubscribes from all data changes
+func (adapter *boardModelAdapter) UnsubscribeAll() (err error) {
+	return nil
+}
---
+++ plugins/boardmodeladapter/boardmodeladapter_test.go
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright 2021 Renesas Inc.
+// Copyright 2021 EPAM Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package boardmodeladapter_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+
+	"aos_vis/plugins/boardmodeladapter"
+)
+
+/*******************************************************************************
+ * Consts
+ ******************************************************************************/
+
+const boardModelVISPath = "Attribute.BoardIdentification.Model"
+
+/*******************************************************************************
+ * Vars
+ ******************************************************************************/
+
+var tmpDir string
+
+/*******************************************************************************
+ * Init
+ ******************************************************************************/
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableTimestamp: false,
+		TimestampFormat:  "2006-01-02 15:04:05.000",
+		FullTimestamp:    true})
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+}
+
+/*******************************************************************************
+ * Main
+ ******************************************************************************/
+
+func TestMain(m *testing.M) {
+	var err error
+
+	tmpDir, err = ioutil.TempDir("", "vis_")
+	if err != nil {
+		log.Fatalf("Error creating tmp dir: %s", err)
+	}
+
+	ret := m.Run()
+
+	if err := os.RemoveAll(tmpDir); err != nil {
+		log.Fatalf("Error removing tmp dir: %s", err)
+	}
+
+	os.Exit(ret)
+}
+
+/*******************************************************************************
+ * Tests
+ ******************************************************************************/
+
+func TestGetName(t *testing.T) {
+	adapter, err := boardmodeladapter.New(generateConfig(boardModelVISPath, path.Join(tmpDir, "boardmodel.txt")))
+	if err != nil {
+		t.Fatalf("Can't create adapter: %s", err)
+	}
+	defer adapter.Close()
+
+	name := adapter.GetName()
+
+	if name != "boardmodeladapter" {
+		t.Errorf("Wrong adapter name: %s", name)
+	}
+}
+
+func TestGetBoardModel(t *testing.T) {
+	boardModelFile := path.Join(tmpDir, "boardmodel.txt")
+	originBoardModel := "TEST_BOARD_MODEL"
+
+	if err := ioutil.WriteFile(boardModelFile, []byte(originBoardModel), 0644); err != nil {
+		t.Fatalf("Can't create boardModel file: %s", err)
+	}
+
+	adapter, err := boardmodeladapter.New(generateConfig(boardModelVISPath, boardModelFile))
+	if err != nil {
+		t.Fatalf("Can't create adapter: %s", err)
+	}
+	defer adapter.Close()
+
+	data, err := adapter.GetData([]string{boardModelVISPath})
+
+	if _, ok := data[boardModelVISPath]; !ok {
+		t.Fatal("boardModel not found in data")
+	}
+
+	boardModel, ok := data[boardModelVISPath].(string)
+	if !ok {
+		t.Fatal("Wrong boardModel data type")
+	}
+
+	if boardModel != originBoardModel {
+		t.Errorf("Wrong boardModel value: %s", boardModel)
+	}
+}
+
+/*******************************************************************************
+ * Private
+ ******************************************************************************/
+
+func generateConfig(visPath, filePath string) (config []byte) {
+	type adapterConfig struct {
+		VISPath  string `json:"VISPath"`
+		FilePath string `json:"FilePath"`
+	}
+
+	var err error
+
+	if config, err = json.Marshal(&adapterConfig{VISPath: visPath, FilePath: filePath}); err != nil {
+		log.Fatalf("Can't marshal config: %s", err)
+	}
+
+	return config
+}
